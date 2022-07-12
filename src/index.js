9bf0ca6f460397b9544819200b266a325864dbe2
Muhammadjon Polvanov <misha.polvanov2706@gmail.com>
2022-07-12 14:25:51 +0500

    chore: task finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let num =
+        n < 0
+            ? String(n * -1)
+                  .split("")
+                  .reverse()
+                  .join("")
+            : String(n).split("").reverse().join("");
+    return Number(num);
+};
